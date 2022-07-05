a0bd3424ebb52fb208e71d0945b7f73441599499
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-07-05 11:44:51 +0200

    Added comments_likes table creation migration
    refs https://github.com/TryGhost/Team/issues/1664

--- core/server/data/schema/schema.js
+++ core/server/data/schema/schema.js
@@ -753,5 +753,11 @@ module.exports = {
         edited_at: {type: 'dateTime', nullable: true},
         created_at: {type: 'dateTime', nullable: false},
         updated_at: {type: 'dateTime', nullable: false}
+    },
+    comments_likes: {
+        id: {type: 'string', maxlength: 24, nullable: false, primary: true},
+        comment_id: {type: 'string', maxlength: 24, nullable: false, unique: false, references: 'comments.id', cascadeDelete: true},
+        member_id: {type: 'string', maxlength: 24, nullable: true, unique: false, references: 'members.id'},
+        created_at: {type: 'dateTime', nullable: false}
     }
 };
--- test/unit/server/data/schema/integrity.test.js
+++ test/unit/server/data/schema/integrity.test.js
@@ -35,7 +35,7 @@ const validateRouteSettings = require('../../../../../core/server/services/route
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = 'b582244f25b7e33b7cdda20427f79702';
+    const currentSchemaHash = '48956e025b2f25924672e5462790f254';
     const currentFixturesHash = '2509ff2c1f6e0293a3c3d84f08593b2f';
     const currentSettingsHash = '0b138cdd40e48b5b7dc4ebac2a7819a7';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
