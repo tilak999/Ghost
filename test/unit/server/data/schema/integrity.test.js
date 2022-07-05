0b24b0404cc3006f569908d96ebe1e11b21c62cb
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-07-05 17:50:01 +0200

    Added `users.comment_notifications` field
    - temporary solution until `user_settings` table is working

--- test/unit/server/data/schema/integrity.test.js
+++ test/unit/server/data/schema/integrity.test.js
@@ -35,7 +35,7 @@ const validateRouteSettings = require('../../../../../core/server/services/route
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = '1c849d899c48d2683a4b2704ef89adcc';
+    const currentSchemaHash = '357834fd1e33bbdf52c1121c418a712d';
     const currentFixturesHash = '2faf307403592522c0fde4204eef2b81';
     const currentSettingsHash = '0b138cdd40e48b5b7dc4ebac2a7819a7';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
