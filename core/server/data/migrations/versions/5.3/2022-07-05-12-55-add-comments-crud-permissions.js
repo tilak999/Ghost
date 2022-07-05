7447ef625e1e6571990de3ddf3899bd8458aae11
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-07-05 15:18:56 +0200

    Added comment CRUD+Moderate permissions

---
+++ core/server/data/migrations/versions/5.3/2022-07-05-12-55-add-comments-crud-permissions.js
@@ -0,0 +1,46 @@
+const {combineTransactionalMigrations, addPermissionWithRoles} = require('../../utils');
+
+module.exports = combineTransactionalMigrations(
+    addPermissionWithRoles({
+        name: 'Browse comments',
+        action: 'browse',
+        object: 'comment'
+    }, [
+        'Administrator'
+    ]),
+    addPermissionWithRoles({
+        name: 'Read comments',
+        action: 'read',
+        object: 'comment'
+    }, [
+        'Administrator'
+    ]),
+    addPermissionWithRoles({
+        name: 'Edit comments',
+        action: 'edit',
+        object: 'comment'
+    }, [
+        'Administrator'
+    ]),
+    addPermissionWithRoles({
+        name: 'Add comments',
+        action: 'add',
+        object: 'comment'
+    }, [
+        'Administrator'
+    ]),
+    addPermissionWithRoles({
+        name: 'Delete comments',
+        action: 'delete',
+        object: 'comment'
+    }, [
+        'Administrator'
+    ]),
+    addPermissionWithRoles({
+        name: 'Moderate comments',
+        action: 'moderate',
+        object: 'comment'
+    }, [
+        'Administrator'
+    ])
+);
