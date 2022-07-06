30b1edf8ad65cc82f7f97170b76cd0c5b70bbdc7
Simon Backx <simon@ghost.org>
2022-07-06 16:58:12 +0200

    Added liked property to comments

--- test/e2e-api/comments/comments.test.js
+++ test/e2e-api/comments/comments.test.js
@@ -1,5 +1,6 @@
 const {agentProvider, mockManager, fixtureManager, matchers} = require('../../utils/e2e-framework');
 const {anyEtag, anyObjectId, anyLocationFor, anyISODateTime, anyUuid, anyNumber, anyBoolean} = matchers;
+require('should');
 
 let membersAgent, membersService, postId, commentId;
 
@@ -73,6 +74,20 @@ describe('Comments API', function () {
         });
 
         it('Can like a comment', async function () {
+            // Check not liked
+            await membersAgent
+                .get(`/api/comments/${commentId}/`)
+                .expectStatus(200)
+                .matchHeaderSnapshot({
+                    etag: anyEtag
+                })
+                .matchBodySnapshot({
+                    comments: new Array(1).fill(commentMatcherWithMember)
+                })
+                .expect(({body}) => {
+                    body.comments[0].liked.should.eql(false);
+                });
+
             // Create a temporary comment
             const {body} = await membersAgent
                 .post(`/api/comments/${commentId}/like/`)
@@ -81,6 +96,20 @@ describe('Comments API', function () {
                     etag: anyEtag
                 })
                 .expectEmptyBody();
+
+            // Check liked
+            await membersAgent
+                .get(`/api/comments/${commentId}/`)
+                .expectStatus(200)
+                .matchHeaderSnapshot({
+                    etag: anyEtag
+                })
+                .matchBodySnapshot({
+                    comments: new Array(1).fill(commentMatcherWithMember)
+                })
+                .expect(({body}) => {
+                    body.comments[0].liked.should.eql(true);
+                });
         });
 
         it('Cannot like a comment multiple times', async function () {
@@ -107,6 +136,20 @@ describe('Comments API', function () {
                     etag: anyEtag
                 })
                 .expectEmptyBody();
+
+            // Check liked
+            await membersAgent
+                .get(`/api/comments/${commentId}/`)
+                .expectStatus(200)
+                .matchHeaderSnapshot({
+                    etag: anyEtag
+                })
+                .matchBodySnapshot({
+                    comments: new Array(1).fill(commentMatcherWithMember)
+                })
+                .expect(({body}) => {
+                    body.comments[0].liked.should.eql(false);
+                });
         });
     });
 });
