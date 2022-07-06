968049a3801f7eb8ef59fd8fdb9c124072fb8c4f
Simon Backx <simon@ghost.org>
2022-07-06 17:15:57 +0200

    Added comment_likes updated_at to schema

--- core/server/data/schema/schema.js
+++ core/server/data/schema/schema.js
@@ -768,7 +768,8 @@ module.exports = {
         id: {type: 'string', maxlength: 24, nullable: false, primary: true},
         comment_id: {type: 'string', maxlength: 24, nullable: false, unique: false, references: 'comments.id', cascadeDelete: true},
         member_id: {type: 'string', maxlength: 24, nullable: true, unique: false, references: 'members.id'},
-        created_at: {type: 'dateTime', nullable: false}
+        created_at: {type: 'dateTime', nullable: false},
+        updated_at: {type: 'dateTime', nullable: false}
     },
     comment_reports: {
         id: {type: 'string', maxlength: 24, nullable: false, primary: true},
